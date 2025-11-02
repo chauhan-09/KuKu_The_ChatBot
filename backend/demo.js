d6da2a5b4bd2d2c8829476e5b8d3ee53946c072f
chauhan-09 <nsc7574@gmail.com>
2025-11-02 22:43:50 +0530

    made changes

---
+++ backend/demo.js
@@ -0,0 +1,37 @@
+// const express = require('express');
+// const app = express();
+
+
+// app.get('/',(req,res) => {
+//     res.send('this is homepage');
+// })
+
+// app.get('/student', (req,res) => {
+//     res.send('this is the student page');
+// })
+
+// app.listen(3000, () => {
+//     console.log("Server is running");
+// })
+
+
+const http = require('http');
+const server = http.createServer((req,res) =>{
+    res.write('Server is running...\n');
+
+    if(req.url === '/home') {
+        res.write('this is the homepage');
+    }
+    else {
+        res.write('sorry request cant be fullfilled');
+    }
+
+    res.end();      //this is mandotory in http to close the connection other user will wait for endtime
+});
+
+
+
+server.listen(3002,()=>{
+    console.log('Server Started');
+})
+
